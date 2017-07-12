58bf0136fd74d28f054ab8559e8abbd737260450
XavierMaita <Jesy@172.26.151.217>
2017-07-12 11:48:33 -0500

    Creacion de la capa de DAO y Correcciones del mapeo a la BD con JPA

--- src/main/java/com/legado/grupo/dao/AsignaturaDAO.java
+++ src/main/java/com/legado/grupo/dao/AsignaturaDAO.java
@@ -1,26 +1,63 @@
 
 package com.legado.grupo.dao;
 
-import com.mysql.jdbc.Connection;
-import com.mysql.jdbc.PreparedStatement;
-import javax.sql.DataSource;
+
+import com.legado.grupo.dom.Asignatura;
+import com.legado.grupo.dom.Carrera;
+import com.legado.grupo.dom.Grupo;
+import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
 @Repository
-//@Service
-public class AsignaturaDAO {
-    /*/@PersistenceContext
-    private EntityManager em;
+public class AsignaturaDAO implements Crud{
     @Autowired
-    private DataSource dataSource;
+    private AsignaturaRepositorio repositorio;
     
-    
-    public void ingresar() throws Exception{
+    public void agregar(Asignatura asignatura, Carrera carrera) {
+        //Una asignatura pertenece a una carrera
+        if(asignatura!=null){
+            asignatura.setCarrera(carrera);
+        }
+        //bidireccionalidad Asignatura-Grupo: Una Asignatura pertenece a un Grupo
+        for(Grupo grupo:asignatura.getGrupos()){
+            grupo.setAsignatura(asignatura);
+            actualizar(asignatura);
+        }
+        repositorio.save(asignatura);
+    }
 
+    @Override
+    public Object buscarPorID(int id) {
+        return repositorio.findOne(id);
+    }
+
+    @Override
+    public void actualizar(Object o) {
+        repositorio.save((Asignatura)o);
+    }
+
+    @Override
+    public void eliminarPorId(int id) {
+        if(existe(id)){
+            repositorio.delete(id);
+        }
+    }
+
+    @Override
+    public void eliminarTodo() {
+        repositorio.deleteAll();
+    }
+
+    @Override
+    public List<Object> listar() {
+        return (List)repositorio.findAll();
     }
     
-    public void modificar () throws Exception{ }
-    public void listar () throws Exception{ }
-    public void eliminar() throws Exception{ }*/
+    @Override
+    public boolean existe(int id) {
+        return repositorio.exists(id);
+    }
+
+   
 }
